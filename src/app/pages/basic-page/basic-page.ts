1dc22967d4876dc74ecf35678f76e404474db477
ciroribba <ciro.ribba@gmail.com>
2025-12-16 10:36:38 -0300

    add content basic page

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPage { }
+export default class BasicPage {
+
+  nameLower = signal('ciro');
+  nameUpper = signal('CIRO');
+  fullName = signal('CiRo DaNiEL');
+}
