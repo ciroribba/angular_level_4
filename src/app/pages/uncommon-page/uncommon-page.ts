af8ebbde85689c35186c2f21b455984d58b4a035
ciroribba <ciro.ribba@gmail.com>
2025-12-16 15:10:58 -0300

    i18nSelectpipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
+import { I18nSelectPipe } from '@angular/common';
 
+const client1 = {
+  name: 'Juan',
+  age: 25,
+  gender: 'male',
+  address: {
+    country: 'Spain',
+    home: 'Calle de la Princesa 123'
+  }
+};
+
+const client2 = {
+  name: 'Ana',
+  age: 30,
+  gender: 'female',
+  address: {
+    country: 'France',
+    home: 'Avenue des Champs-Elysées 456'
+  }
+};
+
+const client3 = {
+  name: 'Pedro',
+  age: 35,
+  gender: 'male',
+  address: {
+    country: 'Italy',
+    home: 'Via Roma 789'
+  }
+};
 @Component({
   selector: 'uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage { }
+export default class UncommonPage { 
+  //i18n Select Pipe
+  client = signal<typeof client1>(client1);
+
+  invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+    } else if (this.client() === client2) {
+      this.client.set(client3);
+    } else {
+      this.client.set(client1);
+    }
+  }
+
+
+}
